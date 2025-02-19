f8bd98f9a86f9d004d8edf396aca830726e00ea3
abdelfadel achraf <abdelfadelachraf1@gmail.com>
2025-02-19 12:13:58 +0100

    AuraStyles - modification

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const rename = require('gulp-rename');
 
 gulp.task('build', () => {
-  return gulp.src('src/**/*.scss')
+  return gulp.src('src/aurastyles.scss') // Only the main file
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('watch', () => {
-  gulp.watch('src/**/*.scss', gulp.series('build'));
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest('dist')) // Save normal CSS
+    .pipe(postcss([cssnano()])) // Minify
+    .pipe(rename({ suffix: '.min' })) // Rename to .min.css
+    .pipe(gulp.dest('dist')); // Save minified CSS
 });
